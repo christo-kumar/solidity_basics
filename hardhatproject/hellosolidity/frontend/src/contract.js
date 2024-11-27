24cee8d8ac117dd8ea094492691cb07d5938ccf2
Christo Kumar <>
2024-11-27 11:43:57 +0530

    added files for frontend project

---
+++ hardhatproject/hellosolidity/frontend/src/contract.js
@@ -0,0 +1,49 @@
+import { ethers } from "ethers";
+
+const address = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
+const abi = [
+  {
+    inputs: [],
+    stateMutability: "nonpayable",
+    type: "constructor",
+  },
+  {
+    inputs: [],
+    name: "getMessage",
+    outputs: [
+      {
+        internalType: "string",
+        name: "",
+        type: "string",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "string",
+        name: "_msg",
+        type: "string",
+      },
+    ],
+    name: "setMessage",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+];
+
+const provider = new ethers.BrowserProvider(window.ethereum);
+
+export const connect = async () => {
+  await provider.send("eth_requestAccounts", []);
+  return getContract();
+};
+
+export const getContract = async () => {
+  const signer = provider.getSigner();
+  const contract = new ethers.Contract(address, abi, signer);
+  return { signer: signer, contract: contract };
+};
